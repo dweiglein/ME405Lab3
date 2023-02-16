16bcc1bcdc4ca385f5c390bf4690c39d3c3b3c0f
dweiglein <123688775+dweiglein@users.noreply.github.com>
2023-02-16 14:16:43 -0800

    Update i2cREPL.py

--- i2cREPL.py
+++ i2cREPL.py
@@ -1,4 +1,20 @@
+import time
+import pyb
+from mma845x import MMA845x
 
-while True:
+if __name__ == "__main__":
     aye = pyb.I2C(1, pyb.I2C.CONTROLLER)
-    pyb.I2C.scan(aye)
+    res = pyb.I2C.scan(aye)
+    print(res)
+    
+    i2c = pyb.I2C (1, pyb.I2C.MASTER, baudrate = 100000)
+    accel1 = MMA845x (i2c, 29)
+    accel1.active()
+    
+    while True:
+        accel = accel1.get_accels()
+        print(accel)
+    
+    
+
+    

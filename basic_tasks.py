1d5ac2b6ba33401098b012a160e86a27793a18e9
jwfraserpoly <jwfraser@calpoly.edu>
2023-02-16 15:02:22 -0800

    push

--- basic_tasks.py
+++ basic_tasks.py
@@ -16,38 +16,33 @@ import cotask
 import task_share
 
 
-def task1_fun(shares):
+def task1(shares):
     """!
     Task which puts things into a share and a queue.
     @param shares A list holding the share and queue used by this task
     """
     # Get references to the share and queue which have been passed to this task
-    my_share, my_queue = shares
-
-    counter = 0
+    accel_q = shares
+    # read accel
     while True:
-        my_share.put(counter)
-        my_queue.put(counter)
-        counter += 1
+        accel_q.put(read_accel)
 
         yield 0
 
 
-def task2_fun(shares):
+def task2(shares):
     """!
     Task which takes things out of a queue and share and displays them.
     @param shares A tuple of a share and queue from which this task gets data
     """
     # Get references to the share and queue which have been passed to this task
-    the_share, the_queue = shares
+    accel_display = shares
+    
 
     while True:
         # Show everything currently in the queue and the value in the share
-        print(f"Share: {the_share.get ()}, Queue: ", end='')
-        while q0.any():
-            print(f"{the_queue.get ()} ", end='')
-        print('')
-
+        print(accel_display.get())
+        
         yield 0
 
 
@@ -55,38 +50,10 @@ def task2_fun(shares):
 # tasks run until somebody presses ENTER, at which time the scheduler stops and
 # printouts show diagnostic information about the tasks, share, and queue.
 if __name__ == "__main__":
-    print("Testing ME405 stuff in cotask.py and task_share.py\r\n"
-          "Press Ctrl-C to stop and show diagnostics.")
+    acQ = task_share.Queue('l', 1000, thread_protect=False, overwrite=True, name='x_accel')
+    task_1 = cotask.Task(task1, name = 'get accel',
+                         priority = 2, period = 500, profile = True, trace=False, shares=(acQ))
+    task_2 = cotask.Task(task2, name = 'print accel',
+                         priority = 1, period = 500, profile = True, trace=False, shares=(acQ))
+    
 
-    # Create a share and a queue to test function and diagnostic printouts
-    share0 = task_share.Share('h', thread_protect=False, name="Share 0")
-    q0 = task_share.Queue('L', 16, thread_protect=False, overwrite=False,
-                          name="Queue 0")
-
-    # Create the tasks. If trace is enabled for any task, memory will be
-    # allocated for state transition tracing, and the application will run out
-    # of memory after a while and quit. Therefore, use tracing only for 
-    # debugging and set trace to False when it's not needed
-    task1 = cotask.Task(task1_fun, name="Task_1", priority=1, period=400,
-                        profile=True, trace=False, shares=(share0, q0))
-    task2 = cotask.Task(task2_fun, name="Task_2", priority=2, period=1500,
-                        profile=True, trace=False, shares=(share0, q0))
-    cotask.task_list.append(task1)
-    cotask.task_list.append(task2)
-
-    # Run the memory garbage collector to ensure memory is as defragmented as
-    # possible before the real-time scheduler is started
-    gc.collect()
-
-    # Run the scheduler with the chosen scheduling algorithm. Quit if ^C pressed
-    while True:
-        try:
-            cotask.task_list.pri_sched()
-        except KeyboardInterrupt:
-            break
-
-    # Print a table of task data and a table of shared information data
-    print('\n' + str (cotask.task_list))
-    print(task_share.show_all())
-    print(task1.get_trace())
-    print('')
